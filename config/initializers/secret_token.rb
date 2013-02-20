23d4f99a4302795b9935b50dc30339e366279448
Inderpal Singh <inderps@thoughtworks.com>
2013-02-20 22:47:27 +0530

    rails app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IphoneMock::Application.config.secret_token = '52a7989a7977d38bfcc4a667bc0b6b781b6aac26f5431da98c2e915ee6a711077853fd76c2312029d7ad1b1e8849d2a28a91e91400692d2d3b33a8bffc9e5fa2'
